62bbfd4648ec64255b12f5ee06a5c5a6c4ca6763
nessalovis <nessa.aidoo@gmail.com>
2018-09-10 10:02:43 +0000

    add C++ grading system project

---
+++ grading.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int marks;
+
+	cout<<"Enter your marks" << endl;
+
+	cin >> marks;
+
+	if (marks >= 90) {
+		cout << "your grade is A+" << endl;
+	}
+	else if (marks >= 80 && marks<90) {
+		cout << "your grade is A" << endl;
+	}
+	else if (marks >= 70 && marks<80) {
+		cout << "your grade is B+" << endl;
+	}
+	else if (marks >= 60 && marks<70) {
+		cout << "your grade is B" << endl;
+	}
+	else if (marks >= 50 && marks<60) {
+		cout << "your grade is C" << endl;
+	}
+	else if (marks >= 40 && marks<50) {
+		cout << "your grade is D" << endl;
+	}
+	else if (marks >= 30 && marks<40) {
+		cout << "your grade is E" << endl;
+	}
+	else if (marks < 30) {
+		cout << "your grade is F" << endl;
+	}
+
+
+
+
+	system("pause");
+}
+
+
